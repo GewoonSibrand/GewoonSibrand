81764cc03e163d93132963428fe44a5133a26bf5
GewoonSibrand <sibrand.premiums@gmail.com>
2019-02-05 15:53:04 +0100

    new page transition

--- page-transition.js
+++ page-transition.js
@@ -54,10 +54,18 @@ var FadeTransition = Barba.BaseTransition.extend({
    */
   
   Barba.Pjax.getTransition = function() {
+
+  var tl = new Timelinemax();
+  tl
+  .to(".screen-wipe-top", 0.5,{y: "50%", repeat: 1, yoyo: true})
+  .to(".screen-wipe-bottom", 0.5,{y: "-50%", repeat: 1, yoyo: true})
+
     /**
      * Here you can use your own logic!
      * For example you can use different Transition based on the current page or link...
      */
-  
+    
     return FadeTransition;
   };
+
+  Barba.Pjax.start();
